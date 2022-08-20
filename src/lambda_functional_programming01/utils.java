80f3e5424b831042a3c7f7084fed32db21af05bf
Ilyas Ceylan <ixirb12@gmail.com>
2022-08-20 23:23:27 +0300

    summer 2022

--- src/lambda_functional_programming01/utils.java
+++ src/lambda_functional_programming01/utils.java
@@ -20,13 +20,28 @@ public class utils {
     public static int kupunuAl(Integer x) {
         return x * x * x;
     }
-    public static double yariyaBol(double x){
-        return x/2.0;
+
+    public static double yariyaBol(double x) {
+        return x / 2.0;
     }
-    public static char sonKarakteriAl(String str){
-        return str.charAt(str.length()-1);
+
+    public static char sonKarakteriAl(String str) {
+        return str.charAt(str.length() - 1);
     }
-    public static char ilkKarakteriAl(String str){
+
+    public static char ilkKarakteriAl(String str) {
         return str.charAt(0);
+    }
+
+    public static int rakamlarToplamiAl(int x) {
+        int temp = x;
+        int birlerBasamagi = 0;
+        int rakamlarToplami = 0;
+        while (x > 0) {
+            birlerBasamagi = temp % 10;
+            rakamlarToplami += birlerBasamagi;
+            temp /= 10;
+        }
+        return rakamlarToplami;
     }
 }
